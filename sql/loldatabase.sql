29f62f9ee02227a66b145a63edb74041243c211f
Mikael Elgstrom <mikael.elgstrom@metropolia.fi>
2019-09-23 19:29:48 +0300

    update

--- sql/loldatabase.sql
+++ sql/loldatabase.sql
@@ -7,11 +7,11 @@ USE LOLDATABASE;
 CREATE TABLE Profile
 
 (
-  Id INT NOT NULL,
-  Name VARCHAR(50) NOT NULL,
-  Psw VARCHAR(25) NOT NULL,
+    Id INT NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    Psw VARCHAR(25) NOT NULL,
 
-  PRIMARY KEY (Id)
+    PRIMARY KEY (Id)
 );
 
 
@@ -19,18 +19,18 @@ CREATE TABLE Profile
 CREATE TABLE GameData
 
 (
-  Champion VARCHAR(50) NOT NULL,
-  Kills INT NOT NULL,
-  Deaths INT NOT NULL,
-  Assist INT NOT NULL,
-  GameId INT NOT NULL,
-  WinLose VARCHAR(10) NOT NULL,
-  Positio VARCHAR(20) NOT NULL,
-  Gpm INT NOT NULL,
-  Id INT NOT NULL,
+    Champion VARCHAR(50) NOT NULL,
+    Kills INT NOT NULL,
+    Deaths INT NOT NULL,
+    Assist INT NOT NULL,
+    GameId INT NOT NULL,
+    WinLose VARCHAR(10) NOT NULL,
+    Positio VARCHAR(20) NOT NULL,
+    Gpm INT NOT NULL,
+    Id INT NOT NULL,
 
-  PRIMARY KEY (GameId),
-  FOREIGN KEY (Id) REFERENCES Profile(Id)
+    PRIMARY KEY (GameId),
+    FOREIGN KEY (Id) REFERENCES Profile(Id)
 );
 
 
@@ -38,20 +38,20 @@ CREATE TABLE GameData
 CREATE TABLE Item
 
 (
-  Slot1 VARCHAR(50) NOT NULL,
-  Slot2 VARCHAR(50) NOT NULL,
-  Slot3 VARCHAR(50) NOT NULL,
-  Slot4 VARCHAR(50) NOT NULL,
-  Slot5 VARCHAR(50) NOT NULL,
-  Slot6 VARCHAR(50) NOT NULL,
-  ItemId INT NOT NULL,
-  GameId INT NOT NULL,
+    Slot1 VARCHAR(50) NOT NULL,
+    Slot2 VARCHAR(50) NOT NULL,
+    Slot3 VARCHAR(50) NOT NULL,
+    Slot4 VARCHAR(50) NOT NULL,
+    Slot5 VARCHAR(50) NOT NULL,
+    Slot6 VARCHAR(50) NOT NULL,
+    ItemId INT NOT NULL,
+    GameId INT NOT NULL,
 
-  PRIMARY KEY (ItemId),
-  FOREIGN KEY (GameId) REFERENCES GameData(GameId)
+    PRIMARY KEY (ItemId),
+    FOREIGN KEY (GameId) REFERENCES GameData(GameId)
 );
 
 
-DROP USER IF EXISTS ryhma10@loldatabase;
-CREATE USER IF NOT EXISTS ryhma10@loldatabase IDENTIFIED BY 'ryhma10';
-
+DROP USER IF EXISTS 'ryhma10'@'localhost';
+CREATE USER IF NOT EXISTS 'ryhma10'@'localhost' IDENTIFIED BY '123456';
+GRANT ALL PRIVILEGES ON loldatabase.* TO 'ryhma10'@'localhost';
